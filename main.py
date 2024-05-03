15d1fb6e1d0651767436913edd0364eb75368e36
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2024-05-03 22:45:26 +0300

    Tetris1

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import pygame
 from pygame import *
-
+from Brick import Brick
 
 window = pygame.display.set_mode((500,700))
 fps = pygame.time.Clock()
 
+brick1 = []
+brick1.append(Brick(300,390,200,200, "Papka/pixil-frame-0 (1).png"))
+brick1.append(Brick(100,250,200,200, "Papka/pixil-frame-0.png"))
+brick1.append(Brick(50,400,200,200, "Papka/pixil-frame-1.png"))
+brick1.append(Brick(250,300,200,200, "Papka/pixil-frame-2.png"))
 
-#backround = pygame.transform.scale(
-  #  pygame.image.load("Papka/"),(700,500)
-#)
+
 
 
 game = True
 while game:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
+
         if event.type == pygame.QUIT:
             pygame.quit()
 
 
 
-
-
-
-
-   # window.blit(backround,(0,0))
-
+    for brick in brick1:
+        brick.draw(window)
 
     pygame.display.flip()
     fps.tick(60)
