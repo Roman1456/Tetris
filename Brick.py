310bbf64080643f8da3c5c87d18a73da53d745d6
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2024-05-04 12:06:49 +0300

    hghghbbbg

--- Brick.py
+++ Brick.py
@@ -1,15 +1,34 @@
 import pygame
-
+import random
 
 class Brick:
-    def __init__(self, x, y, w, h, img):
+    def __init__(self, x, y, w, h, img,speed):
         self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
         self.hitbox = self.photo.get_rect()
         self.hitbox.x = x
         self.hitbox.y = y
+        self.speed = speed
+
 
     def draw(self, window):
         window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
 
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_s]:
+            self.hitbox.y += self.speed
+
+        if keys[pygame.K_d]:
+            self.hitbox.x += self.speed
+
+        if keys[pygame.K_a]:
+            self.hitbox.x -= self.speed
 
 
+
+
+        #self.hitbox.y += self.speed
+        #if self.hitbox.y > 600:
+         #   self.hitbox.y = 0
+         #   self.hitbox.x = random.randint(0, 240)
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pygame
+import random
+from random import *
 from pygame import *
 from Brick import Brick
+
 
 window = pygame.display.set_mode((500,700))
 fps = pygame.time.Clock()
 
+backround = pygame.transform.scale(
+    pygame.image.load("Papka/download.jpg"),(500,700)
+)
+
 brick1 = []
-brick1.append(Brick(300,390,200,200, "Papka/pixil-frame-0 (1).png"))
-brick1.append(Brick(100,250,200,200, "Papka/pixil-frame-0.png"))
-brick1.append(Brick(50,400,200,200, "Papka/pixil-frame-1.png"))
-brick1.append(Brick(250,300,200,200, "Papka/pixil-frame-2.png"))
 
-
+for i in range(1):
+    brick1.append(Brick(300,390,200,200, "Papka/pixil-frame-0 (1).png",5))
+    brick1.append(Brick(100,250,200,200, "Papka/pixil-frame-0.png",5))
+    brick1.append(Brick(50,400,200,200, "Papka/pixil-frame-1.png",5))
+    brick1.append(Brick(250,300,200,200, "Papka/pixil-frame-2.png",5))
+    brick1.append(Brick(100,400,200,200,"Papka/pixil-frame-3.png",5))
+    brick1.append(Brick(56,50,200,200, "Papka/pixil-frame-4.png",5))
+    brick1.append(Brick(350,500,200,200, "Papka/pixil-frame-5.png",5))
 
 
 game = True
@@ -25,8 +35,12 @@ while game:
 
 
 
+
+
+
+    window.blit(backround, (0, 0))
     for brick in brick1:
         brick.draw(window)
-
+        brick.move()
     pygame.display.flip()
     fps.tick(60)
